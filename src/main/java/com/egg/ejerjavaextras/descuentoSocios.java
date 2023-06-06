18958717cb375d5767a776360e2b1e3d0dfaa945
Narracode <zeronarratore@gmail.com>
2023-06-05 19:07:05 -0600

    Una obra social tiene tres clases de socios:  Los socios tipo ‘A’ abonan una cuota mayor, pero tienen un 50% de descuento en todos los tipos de tratamientos.  Los socios tipo ‘B’ abonan una cuota moderada y tienen un 35% de descuento para los mismos tratamientos que los socios del tipo A.  Los socios que menos aportan, los de tipo ‘C’, no reciben descuentos sobre dichos tratamientos. Solicite una letra (carácter) que representa la clase de un socio, y luego un valor real que represente el costo del tratamiento (previo al descuento) y determine el importe en efectivo a pagar por dicho socio.

---
+++ src/main/java/com/egg/ejerjavaextras/descuentoSocios.java
@@ -0,0 +1,53 @@
+package com.egg.ejerjavaextras;
+import java.util.Scanner;
+
+/**
+ *
+ * @author zero
+ */
+public class descuentoSocios {
+
+    public static void main(String[] args) {
+        double desc = ingresarMembresia();
+        System.out.print("Costo del tratamiento > ");
+        double costo = leerCantidad(1,15000);
+        double importe = costo-(costo*desc);
+        System.out.println("Importe para Socios $"+importe);
+    }
+    
+    public static void asiNo() {
+        System.out.println("\nInvalido, intenta otra vez\n");
+    }
+    
+    public static double leerCantidad(double min, double max) {
+        Scanner leer = new Scanner(System.in);
+        System.out.print("Introduce un numero entre "+min+" y "+max+" > ");
+        double x = 0;
+        do {
+            x = leer.nextInt();
+            if (x<min && x>max) {
+                asiNo();
+            }
+        } while (x<min && x>max);
+        return x;
+    }
+    
+    public static double ingresarMembresia() {
+        Scanner leer = new Scanner(System.in);
+        char m;
+        double descuento = 0;
+        do {
+            System.out.print("Ingresa el codigo de tu membresia (A, B o C) > ");
+            m = leer.next().charAt(0);
+            if (m!='A' && m!='B' && m!='C') {
+                asiNo();
+            }
+        } while (m!='A' && m!='B' && m!='C');
+        switch(m) {
+            case 'A': descuento = .50;break;
+            case 'B': descuento = .35;break;
+            case 'C': descuento = 0;break;
+        }
+        return descuento;
+    }
+}
