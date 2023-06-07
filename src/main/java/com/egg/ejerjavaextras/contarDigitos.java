2a3310057c29bb36d024d9a5ad1dd5ac45c1ba98
Narracode <zeronarratore@gmail.com>
2023-06-07 17:37:25 -0600

    Fix poner limite al numero

--- src/main/java/com/egg/ejerjavaextras/contarDigitos.java
+++ src/main/java/com/egg/ejerjavaextras/contarDigitos.java
@@ -8,12 +8,12 @@ import java.util.Scanner;
 public class contarDigitos {
 
     public static void main(String[] args) {
-        Scanner leer = new Scanner(System.in);
         
         System.out.println("Ingresa el monto que te gustaria en tu cuenta bancaria");
-        int cantidad = leer.nextInt();
+        int cantidad = leerCantidad(0,999999999);
         contarDigitos(cantidad);
     }
+
     public static void contarDigitos(int n) {
         int cuenta = 1;
         System.out.print(n+"contiene ");
@@ -22,5 +22,22 @@ public class contarDigitos {
             cuenta++;
         }
         System.out.println(cuenta+" Digitos");
+    }
+    
+    public static void asiNo() {
+        System.out.println("\nInvalido, intenta otra vez\n");
+    }
+
+    public static int leerCantidad(int min, int max) {
+        Scanner leer = new Scanner(System.in);
+        int x = 0;
+        do {
+            System.out.print("Introduce un numero entre "+min+" y "+max+" > ");
+            x = leer.nextInt();
+            if (x<min || x>max) {
+                asiNo();
+            }
+        } while (x<min || x>max);
+        return x;
     }
 }
