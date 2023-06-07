26e53517df99910d463aa9cafa3cc46981f2d7af
Narracode <zeronarratore@gmail.com>
2023-06-06 19:05:18 -0600

    Ejercicio 10 Realice un programa para que el usuario adivine el resultado de una multiplicación entre dos números generados aleatoriamente entre 0 y 10. El programa debe indicar al usuario si su respuesta es o no correcta. En caso que la respuesta sea incorrecta se debe permitir al usuario ingresar su respuesta nuevamente. Para realizar este ejercicio investigue como utilizar la función Math.random() de Java.

---
+++ src/main/java/com/egg/ejerjavaextras/adivinaNumero.java
@@ -0,0 +1,40 @@
+package com.egg.ejerjavaextras;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author zero
+ */
+public class adivinaNumero {
+
+    public static void main(String[] args) {
+        int tgt = (int)((Math.random()*10)*(Math.random()*10)), n=0;
+        System.out.println(tgt);
+        do {
+            n = leerCantidad(1,100);
+            if (n == tgt) {
+                System.out.println("Felicidades, le atinaste! el numero es "+n);
+            } else {
+                System.out.println("Ese no es el numero");
+            }
+        } while (tgt != n);
+    }
+    
+    public static void asiNo() {
+        System.out.println("\nInvalido, intenta otra vez\n");
+    }
+
+    public static int leerCantidad(int min, int max) {
+        Scanner leer = new Scanner(System.in);
+        int x = 0;
+        do {
+            System.out.print("Introduce un numero entre "+min+" y "+max+" > ");
+            x = leer.nextInt();
+            if (x<min || x>max) {
+                asiNo();
+            }
+        } while (x<min || x>max);
+        return x;
+    }
+}
