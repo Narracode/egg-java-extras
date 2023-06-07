bac53bd32934b8112b611a8270ba6657b0ccc345
Narracode <zeronarratore@gmail.com>
2023-06-07 17:36:15 -0600

    Ejercicio 12 Necesitamos mostrar un contador con 3 dígitos (X-X-X), que muestre los números del 00-0 al 9-9-9, con la particularidad que cada vez que aparezca un 3 lo sustituya por una E.

---
+++ src/main/java/com/egg/ejerjavaextras/los3SonE.java
@@ -0,0 +1,27 @@
+package com.egg.ejerjavaextras;
+
+/**
+ *
+ * @author zero
+ */
+public class los3SonE {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k < 10; k++) {
+                    String aux = i+"-"+j+"-"+k;
+                    if (i==3) {
+                        aux = "E-"+j+"-"+k;
+                    } else if(j==3) {
+                        aux = i+"-E-"+k;
+                    } else if(k==3) {
+                        aux = i+"-"+j+"-E";
+                    }
+                    System.out.println(aux);
+                }
+            }
+        }
+    }
+    
+}
