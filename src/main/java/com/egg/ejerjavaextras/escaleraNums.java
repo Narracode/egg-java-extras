a5735c836a70ae42cc00f0d0842b2863e2a718f6
Narracode <zeronarratore@gmail.com>
2023-06-07 19:01:07 -0600

    Ejercicio 13 Crear un programa que dibuje una escalera de números, donde cada línea de números comience en uno y termine en el número de la línea. Solicitar la altura de la escalera al usuario al comenzar.

---
+++ src/main/java/com/egg/ejerjavaextras/escaleraNums.java
@@ -0,0 +1,45 @@
+package com.egg.ejerjavaextras;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author zero
+ */
+public class escaleraNums {
+
+    public static void main(String[] args) {
+        int n = leerCantidad(1,149);
+        
+        dibujaEscalera(n);
+    }
+
+    public static void dibujaEscalera(int escalones) {
+        String aux = "";
+
+        for (int i = 1; i <= escalones; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void asiNo() {
+        System.out.println("\nInvalido, intenta otra vez\n");
+    }
+
+    public static int leerCantidad(int min, int max) {
+        Scanner leer = new Scanner(System.in);
+        int x = 0;
+        
+        do {
+            System.out.print("Introduce un numero entre "+min+" y "+max+" > ");
+            x = leer.nextInt();
+            if (x<min || x>max) {
+                asiNo();
+            }
+        } while (x<min || x>max);
+        return x;
+    }
+}
